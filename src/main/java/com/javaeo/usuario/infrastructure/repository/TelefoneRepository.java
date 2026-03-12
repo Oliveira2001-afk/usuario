a46cf5aed86dd3cd10f8bbf4cdd050112f9d5bc1
ELVIS OLIVEIRA <elvisoliveira0100@hotmail.com>
2026-03-12 12:22:06 -0300

    adicionados metodos get, delet e login

--- src/main/java/com/javaeo/usuario/infrastructure/repository/TelefoneRepository.java
+++ src/main/java/com/javaeo/usuario/infrastructure/repository/TelefoneRepository.java
@@ -1,6 +1,7 @@
 package com.javaeo.usuario.infrastructure.repository;
 
-import com.javaeo.aprendendospring.infrastructure.entity.Telefone;
+
+import com.javaeo.usuario.infrastructure.entity.Telefone;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
